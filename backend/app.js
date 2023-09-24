8fcce6e3d13b5f20228391783f3bc8bb1f14bcf7
Maike Mota <maike_henrique@hotmail.com>
2023-09-24 15:30:30 -0300

    feat: backend configs

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
+const env = require('dotenv').config();
+const cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const jwtService = require("jsonwebtoken");
 
-var indexRouter = require('./routes/index');
+var bodyParser = require('body-parser');
+
+var session = require('express-session');
+
+var SQLiteStore = require('connect-sqlite3')(session);
+
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -14,21 +24,63 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './' })
+}));
+
+
+const middlewareJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  const [_, jwt] = authHeader.split(" ");
+
+
+  // Efetuando a validação do JWT:
+  jwtService.verify(jwt, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+          res.status(403).end();
+          return;
+      }
+      req.user = user;
+      next();
+  });
+};
+
+
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+
+app.use('/users', middlewareJWT, usersRouter);
+app.use('/auth',  authRouter);
+app.use('/posts', middlewareJWT, postsRouter);
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
